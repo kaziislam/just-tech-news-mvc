858ec090d5d114768b4d7bb712b702ead9e356e9
Kazi Islam <kazichaska@gmail.com>
2022-12-11 18:09:35 -0600

    added Post model and REST API routes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+// sequelize.sync({ force: true }) // setting `force: true` will make sure tables drops and recreates 
 sequelize.sync({ force: false })
     .then(() => {
         app.listen(PORT, () => console.log('Now listening!'));
