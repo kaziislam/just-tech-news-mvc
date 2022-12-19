1cddd3a40f7bf4b850fe642abc4482741017ee03
Kazi Islam <kazichaska@gmail.com>
2022-12-18 21:34:01 -0600

    add handlebars and also renamed routes to controllers

---
+++ controllers/home-routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    // res.render('homepage', {
+    //     id: 1,
+    //     post_url: 'https://handlebars.com/guide',
+    //     title: 'Handlebars Docs',
+    //     created_at: new Date(),
+    //     vote_count: 10,
+    //     comments: [{}, {}],
+    //     user: {
+    //         username: 'test_user'
+    //     }
+    // });
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(select count(*) from vote where post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            // pass a single post object into the hostpage tempalate
+            console.log(dbPostData[0]);
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+
+// handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
