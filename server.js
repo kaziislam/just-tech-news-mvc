bcd9ff04739d3a6e4b8fded876893ba4e82f4fa2
Kazi Islam <kazichaska@gmail.com>
2022-12-24 17:37:53 -0600

    optimized and added partials(templates)

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helpers');
 
 // handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
